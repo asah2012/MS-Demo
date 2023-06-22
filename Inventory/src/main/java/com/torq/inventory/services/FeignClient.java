8dd7749321e6076bf9042795c195fe81078202a5
asah2012 <61882773+asah2012@users.noreply.github.com>
2023-06-22 23:35:21 +0530

    Eureka Server
    Eureka Server and Load balancer

--- Inventory/src/main/java/com/torq/inventory/services/FeignClient.java
+++ Inventory/src/main/java/com/torq/inventory/services/FeignClient.java
@@ -4,7 +4,7 @@ import com.torq.inventory.dto.ItemDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@org.springframework.cloud.openfeign.FeignClient(url = "http://localhost:8080/", name = "ITEM-SERVICE")
+@org.springframework.cloud.openfeign.FeignClient(name = "ORDER-SERVICE")
 public interface FeignClient {
     @GetMapping("item/item-code/{itemCodeValue}")
     ItemDto getItemByItemCode(@PathVariable("itemCodeValue") String itemCode);
---
+++ service-registry/src/test/java/com/torq/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.torq.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
